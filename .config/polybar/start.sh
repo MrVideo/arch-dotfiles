cfa900d3a48d2325cb0a67e129176ed762c7c1b3
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2025-05-01 14:18:35 +0200

    Add settings for multimonitor

--- .config/polybar/start.sh
+++ .config/polybar/start.sh
@@ -4,7 +4,15 @@
 polybar-msg cmd quit
 
 # Launch polybar
-echo "---" | tee -a /tmp/polybar.log
-polybar 2>&1 | tee -a /tmp/polybar.log & disown
+# echo "---" | tee -a /tmp/polybar.log
+# polybar 2>&1 | tee -a /tmp/polybar.log & disown
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
 
 echo "Polybar launched..."
